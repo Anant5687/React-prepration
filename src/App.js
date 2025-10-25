5fbe6945dbe9ab769ea3c19bdd382c89cdcff116
Anant Jindal <anant.jindal@kredmint.com>
2025-10-25 20:16:34 +0530

    Created a hook to install useInterval

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Quiz from './components/quiz';
 import EditableTodo from './components/todos/editable';
 import TodoDashboard from './components/todos/todo-dashboard';
+import useInterval from './hooks/useInterval';
 
 function App() {
+  console.log(useInterval(()=> console.log(""), 2000))
   return <EditableTodo />;
 }
 
