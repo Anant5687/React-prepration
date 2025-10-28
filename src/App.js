352cda975d0d8d19585c1a048a7c57b5ffd16c4f
Anant Jindal <anant.jindal@kredmint.com>
2025-10-28 20:07:10 +0530

    Used and previewed that component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import MessageDashboard from './components/message-management/dashboard';
 import Quiz from './components/quiz';
 import EditableTodo from './components/todos/editable';
 import TodoDashboard from './components/todos/todo-dashboard';
+import VsCodeStructure from './components/vs-code-structure';
 import useInterval from './hooks/useInterval';
 
 function App() {
-  return <ImageSlider />;
+  return <VsCodeStructure />;
 }
 
 export default App;
